d29b7f65bc61ae3b3ba0aa86d60ab4f3a2956962
christopheralphonse <christopheralphonse96@gmail.com>
2024-03-09 14:00:57 -0500

    touch styling

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         <div className="md:w-[calc(100%-230px)] w-[calc(100%-60px)] fixed flex items-center justify-between pl-2 pr-6 h-[70px] top-0 md:left-[230px] left-[60px] border-b border-slate-300 bg-slate-50">
             <div className="flex items-center gap-3 cursor-pointer">
                 <IoPersonOutline size={28} className="text-amber-500" />
-                <span className="capitalize text-amber-500 font-semibold md:text-lg text-sm whitespace-nowrap">
+                <span className="capitalize text-amber-500/75 font-semibold md:text-lg text-sm whitespace-nowrap">
                     {boardName || "Board Name"}
                 </span>
                 <IoChevronDownOutline size={16} className="text-amber-500" />
